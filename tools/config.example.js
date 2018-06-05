f8d35cb5ad81b66efb3dc0cd208a64bf8f4fe3f9
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2018-06-05 09:23:58 +0900

    :construction: Add config file

---
+++ tools/config.example.js
@@ -0,0 +1,12 @@
+// Twitter Configuration Example
+// Permissions: Read
+//
+// You must configure this and rename to config.js
+const credentials = {
+  consumer_key: 'YOUR_CONSUMER_KEY',
+  consumer_secret: 'YOUR_CONSUMER_SECRET',
+  access_token: 'YOUR_ACCESS_TOKEN',
+  access_tokenSecret: 'YOUR_ACCESS_TOKEN_SECRET'
+};
+
+export credentials;
--- tools/fetch.js
+++ tools/fetch.js
@@ -2,5 +2,9 @@
 
 // This script fetch some data from Internet.
 // 1. Live schedule from Twitter Statuses, Username and Bio.
-// 2. Timeshift (Record) from LiveStream Providers. (Now, We support YouTube only) 
+// 2. Timeshift (Record) from LiveStream Providers. (Now, We support YouTube only)
 // 3. Icon URL from Twitter.
+
+import credentials from "./config.js";
+
+
